ab8cd34b4d25b7cb24d063688d5e11569a3dc501
Pietro <dibiridim@gmail.com>
2015-08-09 07:07:45 +0000

    More Styling

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,7 +6,7 @@ class CompaniesController < ApplicationController
     @companies = Company.joins("LEFT OUTER JOIN reviews ON reviews.company_id = companies.id")
     .select("companies.id, companies.*, avg(reviews.rate) as average_raiting, count(reviews.id) as number_of_reviews")
     .group("companies.id")
-    .order("average_raiting DESC, number_of_reviews DESC").limit(4)
+    .order("average_raiting DESC, number_of_reviews DESC").limit(8)
   end
 
   # GET /companies/1
