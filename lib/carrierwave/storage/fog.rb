cd088d18f14423185dc53eee8c669aac7a1b1ac2
Pietro <dibiridim@gmail.com>
2015-08-16 13:40:28 +0000

    AWS

---
+++ lib/carrierwave/storage/fog.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],    # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
+  }
+  config.fog_directory  = railsjobs                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
