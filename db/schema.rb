b47525a4b61e365ae8c44384a1e83bc0db0b18e7
Pietro <dibiridim@gmail.com>
2015-08-17 13:19:21 +0000

    Username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812085726) do
+ActiveRecord::Schema.define(version: 20150817130528) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150812085726) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
