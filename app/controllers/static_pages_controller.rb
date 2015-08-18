44c3f980cdd3d69c0142de49c098e49c20c4af50
Pietro <dibiridim@gmail.com>
2015-08-18 09:15:30 +0000

    Fix problem with postgreSQL ordering NULL

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+     @companies3 = Company.joins("INNER JOIN reviews ON reviews.company_id = companies.id")
+    .select("companies.id, companies.*, avg(reviews.rate) as average_raiting, count(reviews.id) as number_of_reviews")
+    .group("companies.id")
+    .order("average_raiting DESC, number_of_reviews DESC")
+    
+    @companies4 = Company.joins("LEFT JOIN reviews ON companies.id = reviews.company_id WHERE reviews.company_id IS NULL")
+    
+    @companies = @companies3 + @companies4
+    
+    
     @companies = Company.joins("LEFT OUTER JOIN reviews ON reviews.company_id = companies.id")
     .select("companies.id, companies.*, avg(reviews.rate) as average_raiting, count(reviews.id) as number_of_reviews")
     .group("companies.id")
