ab3fe860f283932626b382f7091a1bce084e3cad
Pietro <dibiridim@gmail.com>
2015-08-18 08:05:43 +0000

    AWS

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+ if Rails.env.production?
+   CarrierWave.configure do |config|
+        config.fog_credentials = {
+            :provider               => 'AWS',
+            :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+            :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+            # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+        }
+        config.fog_directory  = 'railsjobs'
+      end
+ end
